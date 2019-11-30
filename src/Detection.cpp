5a6679d34dfebb54555efdf94cfac3a4f590ceb9
kamakshijain <kamakshi@terpmail.umd.edu>
2019-11-29 19:31:39 -0500

    Task 2.1 Added stub for class- Detection

---
+++ src/Detection.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file Detection.cpp
+ * Copyright [2019] [Kamakshi Jain] - driver
+ * @date Nov 29, 2019
+ * @brief This is the implementation of the Detection class
+ */
+#include "Detection.hpp"
+
+/**
+ * @brief This is the constructor for the class
+ */
+ 
+Detection::Detection() {
+
+}
+
+/**
+ * @brief It gives the preloaded coordinates for a particular position index.
+ * @params passs the object  coordinates
+ */
+std::vector<double> Detection::getObjectCoords(const int index) {
+
+}
+/**
+ * @brief It detects the position index of a particularly colored object.
+ * @params passs the object color.
+ */
+
+int Detection::colorThresholder(const std::string color) {
+
+}
+/**
+ * @brief It function reads the image
+ * @params passs image.
+ */
+void readImg(const sensor_msgs::ImageConstPtr& msg) {
+
+}
+/**
+ * @brief This is the destructor for the class
+ */
+Detection::~Detection() {
+
+}
