2d56ca34db14b66f846b985ad47791eab6f8484e
Chinj17 <chinmaynj17@gmail.com>
2019-12-08 15:50:29 -0500

    Task 3.3 Added license description and doxygen comments for testDetection.cpp

--- test/testDetection.cpp
+++ test/testDetection.cpp
@@ -1,18 +1,55 @@
-*
- * @file testDetection.cpp
- * 
- * @brief It defines rostest and gtest for Detection class.
+/*
+ * BSD 3-Clause License
  *
- * Copyright [2019] Kamakshi Jain
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ * @file testDetection.cpp
+ * @brief This is the cpp file for testing the Detection class
+ *
+ * @copyright Copyright (c) Fall 2019 ENPM808X
+ *            This project is released under the BSD 3-Clause License.
+ *
+ * @author Chinmay Joshi - Driver
+ * @author Kamakshi Jain - Navigator
+ * @author Sayan Brahma - Design Keeper
+ * @date 12-7-2019
+ *
+ */
 #include <gtest/gtest.h>
 #include "KukaKinematics.hpp"
 #include "Detection.hpp"
 
 /**
  * @brief This is the google test for the first method of the class.
- */ 
+ */
 TEST(DetectionTest, testColorThresholder) {
     // Initialize the Detection and KukaKinematics object
     KukaKinematics robot;
@@ -29,15 +66,15 @@ TEST(DetectionTest, testColorThresholder) {
     ros::spinOnce();
     ros::Duration(1).sleep();
 
-    // Check if the left disc is red colored
-    auto color = test.colorThresholder(robot.LEFT_DISK);
+    // Check if the left slab is red colored
+    auto color = test.colorThresholder(robot.LEFT_SLAB);
     EXPECT_FALSE(color.compare("red"));
 
-    // Check if the right disc is blue colored
-    color = test.colorThresholder(robot.RIGHT_DISK);
+    // Check if the right slab is blue colored
+    color = test.colorThresholder(robot.RIGHT_SLAB);
     EXPECT_FALSE(color.compare("blue"));
 
-    // Check if the any other disc is read
+    // Check if the any other slab is read
     color = test.colorThresholder(robot.HOME);
     EXPECT_TRUE(color.empty());
 }
