1c124c56a5d84b3d81422a4c595a37e71c9538dd
Chinj17 <chinmaynj17@gmail.com>
2019-12-07 22:09:34 -0500

    Task 3.8 Added second test case for Kuka Gripper class

--- test/testGripper.cpp
+++ test/testGripper.cpp
@@ -9,3 +9,25 @@ TEST(KukaGripperTest, testGetGripperState) {
     //
     ASSERT_FALSE(test.getGripperState());
 }
+
+TEST(KukaGripperTest, testGripperToggle) {
+    //
+    KukaGripper test;
+    KukaKinematics robot;
+
+    //
+    robot.sendRobotToPos(robot.HOME);
+    robot.sendRobotToPos(robot.RIGHT_SLAB);
+
+    //
+    test.gripperToggle(true);
+
+    //
+    ASSERT_TRUE(test.getGripperState());
+
+    //
+    test.gripperToggle(false);
+
+    //
+    robot.sendRobotToPos(robot.HOME);
+}
