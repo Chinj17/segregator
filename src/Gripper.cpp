0748875806ccaf944f3a7dd93671e4fa237e684e
Chinj17 <chinmaynj17@gmail.com>
2019-12-07 21:04:56 -0500

    Task 3.7.1 Added the implementation for the constructor of KukaGripper class

--- src/Gripper.cpp
+++ src/Gripper.cpp
@@ -1,4 +1,11 @@
 #include "Gripper.hpp"
 
-//
-KukaGripper::KukaGripper()
+// constructor
+KukaGripper::KukaGripper() {
+    // subscriber
+     gripperSubscriber = n.subscribe("/robot/left_vacuum_gripper/grasping", 10, &KukaGripper::gripperCallback, this);
+    // On State
+     gripperOn = n.serviceClient<std_srvs::Empty>("/robot/left_vacuum_gripper/on");
+    // off State
+     gripperOff = n.serviceClient<std_srvs::Empty>("/robot/left_vacuum_gripper/off");
+}
