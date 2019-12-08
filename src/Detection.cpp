13b815e8303ed4855f9602d85338c6a6172a61bf
Chinj17 <chinmaynj17@gmail.com>
2019-12-07 20:10:42 -0500

    Task 3.6 Added the implementation of constructor

--- src/Detection.cpp
+++ src/Detection.cpp
@@ -1,3 +1,16 @@
 #include "Detection.hpp"
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
+
+Detection::Detection(KukaKinematics & ku, const bool & display) : imgT(n),
+                                                kuka(ku), dispImg(display) {
+
+    imageSubscriber = imgT.subscribe("/camera/image_raw", 1,
+                                                    &Detection::readImg, this);
+    
+    if (dispImg) {
+      cv::namedWindow(OPENCV_WINDOW);
+    }
+
+}
