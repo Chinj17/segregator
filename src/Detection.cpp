60462ad9efc307a26492e4bd9b60c91ec26876a9
Chinj17 <chinmaynj17@gmail.com>
2019-12-08 15:59:42 -0500

    Task 3.3 fixed mistake in the brief

--- src/Detection.cpp
+++ src/Detection.cpp
@@ -32,7 +32,7 @@
 
 /*
  * @file Detection.cpp
- * @brief This is the header file of the Kuka Gripper class
+ * @brief This is the implementation file of the Kuka Detection class
  *
  * @copyright Copyright (c) Fall 2019 ENPM808X
  *            This project is released under the BSD 3-Clause License.
