055bcf49620366c01bc653fde2424703d70408d0
Chinj17 <chinmaynj17@gmail.com>
2019-12-08 16:50:15 -0500

    Task 3.3 added doxygen comments for functions/variables in Gripper.cpp

--- src/Gripper.cpp
+++ src/Gripper.cpp
@@ -45,21 +45,37 @@
 
 #include "Gripper.hpp"
 
-// constructor
+/*
+ * @brief This is the constructor for the class.
+ *
+ * @param No inputs as it is a constructor. It creates a subscriber
+ *        and two service clients.
+ *
+ * @return This is a constructor so it returns nothing.
+ */
 KukaGripper::KukaGripper() {
-    // subscriber
+    // Initializing subscriber
      gripperSubscriber = n.subscribe("/robot/left_vacuum_gripper/grasping", 10, &KukaGripper::gripperCallback, this);
-    // On State
+    // Initializing On State
      gripperOn = n.serviceClient<std_srvs::Empty>("/robot/left_vacuum_gripper/on");
-    // off State
+    // Initializing OFF State
      gripperOff = n.serviceClient<std_srvs::Empty>("/robot/left_vacuum_gripper/off");
 }
 
+/*
+ * @brief This is the first method of the class. It is used to change the
+          state of the vacuum gripper
+ *
+ * @param This method takes state of the gripper.
+ *
+ * @return This method does not return anything.
+ */
 void KukaGripper::gripperToggle(const bool & state) {
     // Call the gripper service
     std_srvs::Empty empty;
     if (state) {
         gripperOn.call(empty);
+        // Check if the gripper was activated
         while (!gripperState) {
             ros::spinOnce();
             ros::Duration(0.1).sleep();
@@ -67,6 +83,7 @@ void KukaGripper::gripperToggle(const bool & state) {
         ros::Duration(2).sleep();
     } else {
         gripperOff.call(empty);
+        // Check if the gripper was deactivated
         while (gripperState) {
             ros::spinOnce();
             ros::Duration(0.1).sleep();
@@ -75,6 +92,14 @@ void KukaGripper::gripperToggle(const bool & state) {
     }
 }
 
+/*
+ * @brief This is the second method of the class. It is used to get the
+ *        gripper state
+ *
+ * @param This method does not take any input.
+ *
+ * @return This method returns the gripper state.
+ */
 bool KukaGripper::getGripperState() {
     return gripperState;
 }
@@ -83,6 +108,13 @@ void KukaGripper::gripperCallback(const std_msgs::Bool & state) {
     gripperState = state.data;
 }
 
+/*
+ * @brief This is the destructor for the class.
+ *
+ * @param This is a destructor so it does not take any inputs.
+ *
+ * @return This is a destructor so it returns nothing.
+ */
 KukaGripper::~KukaGripper() {
     // Can put some warning
 }
