491aaff6354381360dc4262e96c2ad7b09ed6800
Chinj17 <chinmaynj17@gmail.com>
2019-12-07 20:41:51 -0500

    Task 3.7 Added the initial header file for the Kuka Gripper class

---
+++ include/segregator/Gripper.hpp
@@ -0,0 +1,7 @@
+#ifndef INCLUDE_SEGREGATOR_GRIPPER_HPP_
+#define INCLUDE_SEGREGATOR_GRIPPER_HPP_
+
+#include <std_srvs/Empty.h>
+#include <std_msgs/Bool.h>
+#include <ros/ros.h>
+#include <iostream>
