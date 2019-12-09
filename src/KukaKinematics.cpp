1f9e643d9fd43065a3246c09ce10c0d79768d466
Chinj17 <chinmaynj17@gmail.com>
2019-12-08 22:28:12 -0500

    Task 4.7 Corrected cpplint errors KukaKinematics.cpp

--- src/KukaKinematics.cpp
+++ src/KukaKinematics.cpp
@@ -48,7 +48,7 @@
 /**
  * @brief This is the constructor for the class
  */
-KukaKinematics::KukaKinematics() : posJoints{{0,0,0,0,0,0,0},
+KukaKinematics::KukaKinematics() : posJoints{{0, 0, 0, 0, 0, 0, 0},
     {-0.49584801156347513, -0.7282973144476284, -1.0608509979595278,
     0.8929195071031097, -2.4641855236659262, 1.7728411547958132,
     -1.1338010045240487}, {0.6792482988825581,
@@ -63,16 +63,19 @@ KukaKinematics::KukaKinematics() : posJoints{{0,0,0,0,0,0,0},
     -2.8727544970384757, 1.6237417994837084, -0.7114584416147531},
     {2.554736326027861, -0.41894943120107, 0.9373442781635903,
     1.217784288805702, -0.3350002830663925, -1.6425172754590998,
-    0.38282955332744617},{0.11351211197587183, -0.3530547797325959, 0.5,
+    0.38282955332744617}, {0.11351211197587183, -0.3530547797325959, 0.5,
     0.4353723299536111, -0.3255890640361121, -1.813080768567132,
-    0.4517921256510782},{0.44988227072345678, 0.2531091106745061,
+    0.4517921256510782}, {0.44988227072345678, 0.2531091106745061,
     0.6644584598885636, -0.6129732571057295, -1.2563039320840686,
-    0.8961754475543316, 0.9061755708947427}, {0, 0, 0, 0, 0, 0, 0}, {-1.570796327, 0.2531091106745061, 0.6644584598885636, -0.6129732571057295, -1.2563039320840686,
-    0.8961754475543316, 0.9061755708947427}} {
+    0.8961754475543316, 0.9061755708947427}, {0, 0, 0, 0, 0, 0, 0},
+    {-1.570796327, 0.2531091106745061, 0.6644584598885636,
+    -0.6129732571057295, -1.2563039320840686, 0.8961754475543316,
+     0.9061755708947427}} {
     // Initialize the trajectory message attributes
     initializeTrajectoryPoint();
     // Initialize the publisher
-    jointPublisher = n.advertise<trajectory_msgs::JointTrajectory>("/iiwa/PositionJointInterface_trajectory_controller/command", .10);
+    jointPublisher = n.advertise<trajectory_msgs::JointTrajectory>(
+             "/iiwa/PositionJointInterface_trajectory_controller/command", .10);
 }
 
 /*
@@ -113,8 +116,8 @@ void KukaKinematics::sendRobotToPos(const States & state) {
     // Define the jointCommands variable
     jointCommands.header.stamp = ros::Time::now();
     jointCommands.header.frame_id = statesStr.at(num);
-    int i =0;
-    while(i < numJoints) {
+    int i = 0;
+    while (i < numJoints) {
         jointCommands.points[0].positions[i] = posJoints[num][i];
         i = i+1;
     }
